a5af1462dd9e71d46ccab1a3a893d32202c76ebc
lahirumenik <115687865+lahirumenik@users.noreply.github.com>
2024-10-28 23:45:10 +0530

    Cpp template for fast coding

---
+++ general_template.cpp
@@ -0,0 +1,72 @@
+
+#include<bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#define lli long long int
+#define li long int
+#define ld long double
+using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_set_duplicate = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll;
+template<typename... T>
+void see(T&... args) { ((cin >> args), ...);}
+#define sz size()
+#define seea(a,x,y) for(int i=x;i<y;i++){cin>>a[i];}
+#define seev(v,n) for(int i=0;i<n;i++){int x; cin>>x; v.push_back(x);}
+#define sees(s,n) for(int i=0;i<n;i++){int x; cin>>x; s.insert(x);}
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii>
+#define vc vector
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define setpr(x) cout<<setprecision(x)<<fixed
+#define all(x) x.begin(),x.end()
+const int mod = 1e9+7;
+#define INF 1e9
+#define int long long
+#define rep(i,a,b) for (int i=a; i<b; ++i)
+#define rev(i,a,b) for (int i=a; i>b; --i)
+#define pb push_back
+#define F first
+#define S second
+#define tiii tuple<int,int,int>
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+const long long mod_inverse_2 = 500000004;
+const ll inf = 1LL<<62;
+
+
+ 
+ 
+void solve(){
+    
+
+    
+ 
+}
+ 
+signed main() {
+    IOS;
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        //cout<<'\n';
+    }
+ 
+ 
+}
+
